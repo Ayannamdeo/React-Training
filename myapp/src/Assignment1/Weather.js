a8e107bedf3246b6f3544b69913085441d9056ef
Ayan Namdeo <ayan.namdeo@ayannamdeor-424.successivetech.com>
2024-03-28 18:23:59 +0530

    updated

--- myapp/src/Assignment1/Weather.js
+++ myapp/src/Assignment1/Weather.js
@@ -8,3 +8,5 @@ export function Weather(props){
         return <h1>it's cold today</h1>
     }
 }
+
+
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -15,6 +15,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <hr/>
+    <br/>
+    
 
     <UserCard 
         name="person1"
@@ -26,17 +29,26 @@ root.render(
         email="person2@gmail.com"
         url = "https://i.imgur.com/OKS67lh.jpg"
               />
-
+    <hr/>
+    <br/>  
     <Weather temperature={26}/>
     <Weather temperature={8}/>
+    <hr/>
+    <br/>
 
     <TaskList tasks={['task1', 'task2', 'task3', 'task4']}/>
+    <hr/>
+    <br/>
 
     <Counter />
+    <hr/>
+    <br/>
 
     <Button text='Submit' color="blue"/>
     <Button text='Cancel' color="red"/>
     <Button text='Save' color="Green"/>
+    <hr/>
+    <br/>
 
 
   </React.StrictMode>
