865dd4a3da694f4a8ff7e39df2010e9c4a9e7802
Ayan Namdeo <ayan.namdeo@ayannamdeor-424.successivetech.com>
2024-04-22 18:09:27 +0530

    completed ass6

--- myapp/src/assignments/Assignment6/Assignment6.js
+++ myapp/src/assignments/Assignment6/Assignment6.js
@@ -2,14 +2,17 @@ import { Counter } from "./Q3/Counter";
 import { LoginForm } from "./Q4/LoginForm";
 import { Q5 } from "./Q5/Q5";
 import { Q6 } from "./Q6/Q6";
+import { Q1_Q2 } from "./Q1_Q2/Q1_Q2";
 
 export function Assignment6(){
     return (
         <>
+        
         {/* <Counter/> */}
         {/* <LoginForm /> */}
         {/* <Q5 /> */}
-        <Q6 />
+        {/* <Q6 /> */}
+        <Q1_Q2 />
         </>
     );
 
